1fe831218efed7a64fa6cefb3c0628c67aaafda2
Chris Hoage <iam@chrishoage.com>
2015-09-25 13:25:24 -0700

    remove example app stuff

--- src/js/components/App/App.js
+++ src/js/components/App/App.js
@@ -1,6 +1,4 @@
 import React, { Component, propTypes } from 'react'
-import Counter from 'components/Counter'
-import * as colors from './colors'
 
 import './App.scss'
 
@@ -8,8 +6,7 @@ export default class App {
   render() {
     return (
       <div>
-        <Counter increment={1} color={colors.COUNTER_COLOR} />
-        <Counter increment={10} color={colors.COUNTER_COLOR_ALT} />
+        hello world
       </div>
     )
   }
